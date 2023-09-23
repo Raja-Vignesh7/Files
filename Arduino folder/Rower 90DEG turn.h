e2bcc6542dce29cef48fd5d866fffed6865ec544
Raja-Vignesh7 <126546616+Raja-Vignesh7@users.noreply.github.com>
2023-09-23 11:18:55 +0530

    Update Rower 90DEG turn.h

--- Arduino folder/Rower 90DEG turn.h
+++ Arduino folder/Rower 90DEG turn.h
@@ -1,4 +1,5 @@
 #include <AFMotor.h>
+#include <math.h>
 int state=0;
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
@@ -19,20 +20,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 	uint8_t i;
-  // To rotate 90DEG towards right
-  if(state==1){ 
+
+    // To rotate 90DEG towards right
+  
+  if(state==1){
     motor1.run(FORWARD);
     motor2.run(FORWARD);
     motor3.run(BACKWARD);
     motor4.run(BACKWARD);
-    for (i=0; i<140; i++) 
-	{
-		motor1.setSpeed(i);
-    motor2.setSpeed(i);
-    motor3.setSpeed(i);
-    motor4.setSpeed(i);  
-		delay(10);
-	}
+  motor1.setSpeed(100);
+  motor2.setSpeed(100);
+  motor3.setSpeed(100);
+  motor4.setSpeed(100);
+  delay(500);
   state=0;
   }
 	// Turn on motor
